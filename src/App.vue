9c428ece25fce8cd9d887f613cdea6e372c9e9b8
jozsef-sogor <44443711+jozsef-sogor@users.noreply.github.com>
2019-12-03 14:53:40 +0100

    Cleaning up the files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <div id="nav">
-            <router-link to="/information">Information</router-link> 
+            <router-link to="/information">Information</router-link>
             <router-link to="/">Map</router-link>
             <router-link to="/task">Tasks</router-link>
         </div>
@@ -9,25 +9,4 @@
     </div>
 </template>
 
-<style lang="scss">
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
-}
-</style>
+<style lang="scss"></style>
