fc1c437b14984ed9d5b005471bfb17a5a33d063d
Silvia34 <eaasart@students.eaaa.dk>
2019-12-04 15:32:29 +0100

    Update Welcome.vue

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,9 +1,39 @@
 <template>
-    <div class="welcome">Welcome</div>
+    <div class="welcome">
+        <div>
+            <v-carousel :show-arrows="false">
+                <v-carousel-item
+                    v-for="(item, i) in items"
+                    :key="i"
+                    :src="item.src"
+                ></v-carousel-item>
+            </v-carousel>
+        </div>
+    </div>
 </template>
 
 <style lang="scss"></style>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    src:
+                        'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+                }
+            ]
+        };
+    }
+};
 </script>
