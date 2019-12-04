2d3d5debb319c485ba249228873d6a933c5b5175
GabiRod <rodickova.gabriela@gmail.com>
2019-12-04 13:31:20 +0100

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <template>
-    <div id="app">
-        <Navbar v-if="!$route.meta.hideNavigation" />
-        <Heading />
-        <router-view />
-    </div>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <div class="d-flex align-center">
+                <v-img
+                    alt="Vuetify Logo"
+                    class="shrink mr-2"
+                    contain
+                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                    transition="scale-transition"
+                    width="40"
+                />
+
+                <v-img
+                    alt="Vuetify Name"
+                    class="shrink mt-1 hidden-sm-and-down"
+                    contain
+                    min-width="100"
+                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+                    width="100"
+                />
+            </div>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-content>
+            <HelloWorld />
+        </v-content>
+    </v-app>
 </template>
 
-<style lang="scss"></style>
-
 <script>
-import './styles/reboot.module.scss';
-import Navbar from './components/Navbar.vue';
-import Heading from './components/Heading.vue';
+import HelloWorld from './components/HelloWorld';
 
 export default {
+    name: 'App',
+
     components: {
-        Navbar,
-        Heading
-    }
+        HelloWorld
+    },
+
+    data: () => ({
+        //
+    })
 };
 </script>
