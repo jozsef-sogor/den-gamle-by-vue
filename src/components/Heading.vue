8d11083a4089993e4b2ff3c4987f27dddc3d7286
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-12-04 12:08:55 +0100

    add Heading.vue

---
+++ src/components/Heading.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="heading">
+        <h1>Test</h1>
+    </div>
+</template>
+
+<style lang="scss">
+.heading {
+    font-size: 2rem;
+    border: 1px solid black;
+
+    h1 {
+        margin: 0;
+    }
+}
+</style>
+
+<script>
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.css';
+
+export default {
+    name: 'head'
+};
+</script>
