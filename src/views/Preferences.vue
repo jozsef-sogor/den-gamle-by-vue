a52f7885b4839cf03cac407efa9fb429dbde28c2
GabiRod <rodickova.gabriela@gmail.com>
2019-12-05 17:23:00 +0100

    updates of preferences 5.12.

--- src/views/Preferences.vue
+++ src/views/Preferences.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="preferences">
         <h1>Preferences</h1>
-        <v-container fluid>
+        <v-container fluid class="container-border">
             <v-row>
                 <v-col cols="12">
-                    <h5>
+                    <h3>
                         How much time do you have?
-                    </h5>
+                    </h3>
                     <div class="slider-flex">
                         <p class="hour">1h</p>
                         <v-slider
@@ -25,25 +25,63 @@
             </v-row>
         </v-container>
 
-        <v-col cols="12">
-            <h5>Who are you visiting with?</h5>
+        <v-container class="container-border">
+            <h3>
+                Which one do you prefer?
+            </h3>
 
-            <v-checkbox
-                v-model="Iamalone"
-                class="mx-2"
-                label="I am alone"
-            ></v-checkbox>
-            <v-checkbox
-                v-model="Partner"
-                class="mx-2"
-                label="Partner"
-            ></v-checkbox>
-            <v-checkbox
-                v-model="Children"
-                class="mx-2"
-                label="Children"
-            ></v-checkbox>
-        </v-col>
+            <v-item-group multiple>
+                <v-row>
+                    <v-col
+                        v-for="item in items"
+                        :key="item"
+                        cols="6"
+                        md="1"
+                        class="d-flex align-center flex-row"
+                    >
+                        <v-item v-slot:default="{ active, toggle }">
+                            <v-card
+                                :color="active ? 'primary' : ''"
+                                class="flex-grow-1 text-center img-bck"
+                                height="100px"
+                                width="100px"
+                                @click="toggle"
+                            >
+                                <v-img
+                                    :src="`${item.img}`"
+                                    class="img-bck"
+                                ></v-img>
+                                <div class="title-centered">
+                                    {{ item.title }}
+                                </div>
+                            </v-card>
+                        </v-item>
+                    </v-col>
+                </v-row>
+            </v-item-group>
+        </v-container>
+
+        <v-container class="container-border">
+            <v-col cols="12">
+                <h3>Who are you visiting with?</h3>
+
+                <v-checkbox
+                    v-model="Iamalone"
+                    class="mx-2"
+                    label="I am alone"
+                ></v-checkbox>
+                <v-checkbox
+                    v-model="Partner"
+                    class="mx-2"
+                    label="Partner"
+                ></v-checkbox>
+                <v-checkbox
+                    v-model="Children"
+                    class="mx-2"
+                    label="Children"
+                ></v-checkbox>
+            </v-col>
+        </v-container>
     </div>
 </template>
 
@@ -75,6 +113,46 @@ h5 {
 .v-input--selection-controls__ripple {
     border: black;
 }
+.v-card {
+    transition: opacity 0.4s ease-in-out;
+    display: grid;
+    border: rgba(245, 173, 90, 0.582) solid 3px;
+}
+
+.img-bck {
+    position: relative;
+}
+.title-centered {
+    z-index: 1;
+    color: white;
+    position: absolute;
+    display: grid;
+    place-self: center;
+}
+
+.v-application {
+    .primary {
+        background-color: $orange !important;
+        border-color: $orange !important;
+    }
+    .accent--text {
+        color: $orange !important;
+        caret-color: $orange !important;
+    }
+    .container-border {
+        max-width: 80%;
+        margin: 15px 5px;
+        border-style: solid;
+        border-radius: 15px;
+        border-width: 3px;
+        border-color: $blue;
+        box-shadow: 5px 5px 15px rgba(163, 163, 163, 0.575);
+    }
+}
+
+.show-btns {
+    color: rgba(255, 255, 255, 1);
+}
 </style>
 
 <script>
@@ -83,7 +161,19 @@ export default {
         Iamalone: false,
         Partner: false,
         Children: false,
-        slider: 1
+        slider: 1,
+        items: [
+            {
+                title: 'Early centuries',
+                img:
+                    'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+            },
+            {
+                title: 'Modern centuries',
+                img:
+                    'https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+            }
+        ]
     })
 };
 </script>
