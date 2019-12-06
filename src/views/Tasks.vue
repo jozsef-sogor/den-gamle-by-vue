1572c72a6ceecdbd1bb948d6422973a210446765
GabiRod <rodickova.gabriela@gmail.com>
2019-12-06 15:14:18 +0100

    taskc collapse

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -13,19 +13,48 @@
                 <h3>{{ Math.ceil(knowledge) }}/{{ done }}</h3>
             </v-progress-linear>
         </div>
+        <div class="expansion-panel">
+            <v-expansion-panels class="mb-6">
+                <v-expansion-panel>
+                    <v-expansion-panel-header expand-icon="mdi-menu-down">
+                        1864 zone
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        Backery
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+                <v-expansion-panel>
+                    <v-expansion-panel-header expand-icon="mdi-menu-down">
+                        1927 zone
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        Restaurant
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+                <v-expansion-panel>
+                    <v-expansion-panel-header expand-icon="mdi-menu-down">
+                        1974 zone
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        Bar
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 .progress {
     margin-left: 10px;
+    margin-top: 20px;
     margin-right: 10px;
 }
 .orange-color {
     color: $orange;
     position: absolute;
-    top: 15px;
-    left: 15px;
+    top: 35px;
+    left: 10px;
 }
 </style>
 
