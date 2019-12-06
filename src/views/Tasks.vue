65d978085e193a23bd1cbe8d817e76488ff4d013
GabiRod <rodickova.gabriela@gmail.com>
2019-12-06 15:11:41 +0100

    tasks page created

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,11 +1,41 @@
 <template>
     <div class="tasks">
+        <v-icon x-large class="orange-color">mdi-arrow-left-circle</v-icon>
         <h1>Tasks</h1>
+        <div class="progress">
+            <v-progress-linear
+                v-model="knowledge"
+                height="20"
+                reactive
+                color="#e58c4f"
+                track-color="#e58c4f75"
+            >
+                <h3>{{ Math.ceil(knowledge) }}/{{ done }}</h3>
+            </v-progress-linear>
+        </div>
     </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.progress {
+    margin-left: 10px;
+    margin-right: 10px;
+}
+.orange-color {
+    color: $orange;
+    position: absolute;
+    top: 15px;
+    left: 15px;
+}
+</style>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            knowledge: 25,
+            done: 10
+        };
+    }
+};
 </script>
