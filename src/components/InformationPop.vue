b07ebb9eba62bfdacc415608bcc66390b78bd64f
Silvia34 <eaasart@students.eaaa.dk>
2019-12-09 09:52:14 +0100

    changes information-pop

--- src/components/InformationPop.vue
+++ src/components/InformationPop.vue
@@ -6,12 +6,59 @@
             {{ house.id }}
         </article> -->
         <template v-if="filteredHouse" v-on:pin-clicked="test" ref="infopop">
-            {{ filteredHouse.description }}
+            <div class="titlbkg">
+                <router-link to="/map" class="decoration">
+                    <v-icon class="close">mdi-close</v-icon>
+                </router-link>
+                <h2 class="titleHouse">{{ filteredHouse.title }}</h2>
+            </div>
+            <div class="textHouse">{{ filteredHouse.description }}</div>
+            <router-link to="/tasks" class="decoration">
+                <v-btn block class="btnQuiz">
+                    Take a quiz
+                </v-btn>
+            </router-link>
         </template>
     </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.information-pop {
+    position: absolute;
+    z-index: 5;
+    background-color: white;
+    height: 100vh;
+    width: 100vw;
+    .close {
+        color: white;
+        position: absolute;
+        top: 5px;
+        left: 275px;
+    }
+    .titlbkg {
+        background-color: $blue;
+    }
+    .titleHouse {
+        padding: 20px 20px 20px 20px;
+        color: white;
+        text-align: center;
+    }
+    .textHouse {
+        font-family: $body-font;
+        font-size: 16px;
+        margin: 30px 30px 40px 30px;
+        text-align: justify;
+    }
+    .theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+        background-color: $orange;
+        color: white;
+        font-family: $heading-font;
+    }
+    .decoration {
+        text-decoration: none;
+    }
+}
+</style>
 
 <script>
 export default {
