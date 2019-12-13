06d8759e209d8b4c712fbb656e96efecf83ee813
jozsef-sogor <44443711+jozsef-sogor@users.noreply.github.com>
2019-12-13 14:59:34 +0100

    animation added

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,14 +1,50 @@
 <template>
     <div id="app">
-        <information-pop
-            :houseId="childData"
-            v-if="childData"
-            @close-clicked="closePopUp"
-        />
-
+        <transition name="slide">
+            <information-pop
+                :houseId="childData"
+                v-if="childData"
+                @close-clicked="closePopUp"
+            />
+        </transition>
         <google-map @pin-clicked="openPopUp" @positionMatch="openPopUp" />
     </div>
 </template>
+
+<style lang="scss">
+.slide-enter-active {
+    -webkit-animation: slide-in-bottom 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+        both;
+    animation: slide-in-bottom 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+.slide-leave-active {
+    -webkit-animation: slide-in-bottom 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+        reverse both;
+    animation: slide-in-bottom 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse
+        both;
+}
+
+@-webkit-keyframes slide-in-bottom {
+    0% {
+        -webkit-transform: translateY(100%);
+        transform: translateY(100%);
+    }
+    100% {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
+}
+@keyframes slide-in-bottom {
+    0% {
+        -webkit-transform: translateY(100%);
+        transform: translateY(100%);
+    }
+    100% {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
+}
+</style>
 
 <script>
 import googleMap from '../components/googleMap';
